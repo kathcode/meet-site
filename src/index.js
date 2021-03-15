0cec8e5d404a039e8e6fbefe4159bc997d16a4ea
kathcode <kathmene27@gmail.com>
2021-03-15 17:37:22 -0500

    Add router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+import Header from './shared/components/Header';
+import List from './views/List';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Switch>
+        <Router exact path="/detail/:id">
+          Detail
+        </Router>
+        <Route exact path="/">
+          <List />
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
